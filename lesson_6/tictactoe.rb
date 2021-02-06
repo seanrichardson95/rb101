2efffb83b1b104b15d2af2ca289d2b00e2d4d5ec
Sean Richardson <sean.richardson.umd18@gmail.com>
2021-02-06 04:20:44 +0000

    Tic Tac Toe

---
+++ lesson_6/tictactoe.rb
@@ -0,0 +1,184 @@
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+FIRST = 'Computer'
+BEST_SQUARE = 5
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                [[1, 5, 9], [3, 5, 7]]              # diagonals
+
+require 'pry'
+
+def prompt(msg)
+  puts "=> " + msg
+end
+
+# rubocop:disable Metrics/AbcSize
+def display_board(brd)
+  system 'clear'
+  puts "You're a #{PLAYER_MARKER}. The Computer is a #{COMPUTER_MARKER}."
+  puts ""
+  puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
+  puts "     |     |"
+  puts ""
+end
+# rubocop:enable Metrics/AbcSize
+
+def initialize_board
+  new_board = {}
+  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
+  new_board
+end
+
+def empty_squares(brd)
+  brd.keys.select {|num| brd[num] == INITIAL_MARKER}
+end
+
+def player_places_piece!(brd)
+  square = ''
+  loop do
+    prompt "Choose a square (#{joinor(empty_squares(brd))}):"
+    square = gets.chomp
+    square = square.to_i.to_f == square.to_f ? square.to_i : nil
+    break if empty_squares(brd).include?(square)
+    puts "Sorry, that's not a valid choice"
+  end
+    
+  brd[square] = PLAYER_MARKER
+end
+
+def at_risk_square(brd, marker)
+  other_marker = marker == PLAYER_MARKER ? COMPUTER_MARKER : PLAYER_MARKER
+  WINNING_LINES.each do |line|
+    if (brd.values_at(*line).count(marker) == 2) &&
+       (brd.values_at(*line).count(other_marker) != 1)
+      return brd.select {|sq, val| line.include?(sq) && val == " "}.keys[0]
+    end
+  end
+  nil
+end
+
+# rubocop: disable Layout/LineLength
+def computer_places_piece!(brd)
+  square = empty_squares(brd).sample
+  square = BEST_SQUARE if brd[BEST_SQUARE] == ' '
+  # I know rubocop thinks these lines are too long, but I like the way they look
+  square = at_risk_square(brd, PLAYER_MARKER) if !!at_risk_square(brd, PLAYER_MARKER)
+  square = at_risk_square(brd, COMPUTER_MARKER) if !!at_risk_square(brd, COMPUTER_MARKER)
+  brd[square] = COMPUTER_MARKER
+end
+# rubocop: enable Layout/LineLength
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  !!detect_winner(brd) # !! forcibly turns this value into a boolean
+end
+
+def detect_winner(brd)
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+      return 'Player'
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+      return 'Computer'
+    end
+  end
+  nil
+end
+
+def update_score!(scoreboard, winner)
+  scoreboard[winner.downcase.to_sym] += 1
+end
+
+def joinor(array, delimiter = ', ', joiner = 'or')
+  case array.size
+  when 0 then ''
+  when 1 then array[0].to_s
+  when 2 then "#{array[0]} #{joiner} #{array[1]}"
+  else
+    array[-1] = "#{joiner} #{array[-1]}"
+    array.join(delimiter)
+  end
+end
+
+def choose_first
+  loop do
+    prompt "You get to choose who goes first"
+    prompt "Type '1' for 'Player' and type '2' for 'Computer'"
+    answer = gets.chomp
+    goes_first = answer == '2' ? 'Computer' : 'Player'
+    return goes_first if answer == '1' || answer == '2'
+    puts "I'm sorry, please enter a valid response."
+  end
+end
+
+def place_piece!(board, current_player)
+  if current_player == 'Player'
+    player_places_piece!(board)
+  else
+    computer_places_piece!(board)
+  end
+end
+
+def alternate_player(current_player)
+  current_player == 'Player' ? 'Computer' : 'Player'
+end
+# main loop
+loop do
+  
+  # scoreboard initialized
+  scoreboard = { player: 0, computer: 0 }
+  
+  # new game loop
+  loop do                         
+    board = initialize_board
+    current_player = FIRST
+    current_player = choose_first if FIRST == 'Choose'
+    # gameplay loop
+    loop do
+      display_board(board)
+      place_piece!(board, current_player)
+      current_player = alternate_player(current_player)
+      break if someone_won?(board) || board_full?(board)
+    end
+    
+    display_board(board)
+    
+    if someone_won?(board)
+      prompt "#{detect_winner(board)} won!"
+      update_score!(scoreboard, detect_winner(board))
+    else
+      prompt "It's a tie!"
+    end
+    
+    prompt "The Computer has #{scoreboard[:computer]} win(s)"
+    prompt "You have #{scoreboard[:player]} win(s)"
+    
+    if scoreboard.values.any? {|wins| wins == 5}
+      prompt "#{detect_winner(board)} wins the series!"
+      break
+    else
+      prompt "The first to 5 wins the series!"
+      prompt "Hit 'enter' to continue to the next game."
+      gets
+    end
+    
+  end
+  
+  prompt "Play again? (y/n)"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt "Thanks for playing tic tac toe! Goodbye"
